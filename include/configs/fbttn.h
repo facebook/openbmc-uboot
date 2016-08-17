4f5847231b06263d04f93c9445950b27ecbc3351
Jubin Mehta <jmehta@fb.com>
2016-08-17 11:54:06 -0700

    Add fbttn support in u-boot 2016.07
    Reviewed By: theopolis, sdasari73
    
    Differential Revision: D3729253
    
    fbshipit-source-id: e82a5b4

---
+++ include/configs/fbttn.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2012-2020  ASPEED Technology Inc.
+ * Ryan Chen <ryan_chen@aspeedtech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __FBTTN_CONFIG_H
+#define __FBTTN_CONFIG_H
+
+#define CONFIG_IDENT_STRING " fbttn-v0.1"
+#define CONFIG_FBTTN 1
+
+#define CONFIG_BOOTARGS          "debug console=ttyS0,57600n8 root=/dev/ram rw"
+#define CONFIG_UPDATE            "tftp 80800000 ast2500.scr; so 80800000'"
+#define CONFIG_BOOTCOMMAND       "bootm 20080000 20480000"
+#define CONFIG_BOOTFILE          "flash-fbttn"
+
+#define CONFIG_SYS_NS16550_COM1         AST_UART0_BASE
+#define CONFIG_CONS_INDEX               1
+#define CONFIG_BAUDRATE                 57600
+
+/*
+ * EEPROM configuration
+ */
+#define CONFIG_SYS_I2C_EEPROM_ADDR_LEN  2
+#define CONFIG_SYS_I2C_EEPROM_ADDR  0xa0
+
+/*
+ * NIC configuration
+ */
+#define CONFIG_MAC1_PHY_SETTING     2
+#define CONFIG_MAC2_PHY_SETTING     0
+#define CONFIG_ASPEED_MAC_NUMBER  1
+#define CONFIG_ASPEED_MAC_CONFIG  1 // config MAC1
+
+#include "facebook_common.h"
+#include "ast2500_common.h"
+
+#endif  /* __FBTTN_CONFIG_H */

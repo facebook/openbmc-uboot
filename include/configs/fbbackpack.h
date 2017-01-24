625f64c3726391049e724d951652ba347153d82a
Tian Fang <tfang@fb.com>
2017-01-23 18:37:48 -0800

    Configurations and UART macro for switches on ast1250
    Reviewed By: sdasari73
    
    Differential Revision: D4503752
    
    fbshipit-source-id: 33a5598

--- include/configs/fbbackpack.h
+++ include/configs/fbbackpack.h
@@ -5,23 +5,23 @@
  * SPDX-License-Identifier: GPL-2.0+
  */
 
-#ifndef __FBWEDGE100_CONFIG_H
-#define __FBWEDGE100_CONFIG_H
+#ifndef __FBBACKPACK_CONFIG_H
+#define __FBBACKPACK_CONFIG_H
 
-#define CONFIG_IDENT_STRING "\nFacebook Wedge100 OpenBMC"
-#define CONFIG_FBWEDGE100 1
+#define CONFIG_IDENT_STRING "\nFacebook Backpack OpenBMC"
+#define CONFIG_FBBACKPACK 1
 
 #define CONFIG_SYS_LONGHELP
 
-#define CONFIG_BOOTARGS   "debug console=ttyS2,9600n8 root=/dev/ram rw"
-#define CONFIG_BOOTFILE   "flash-wedge100"
+#define CONFIG_BOOTARGS   "debug console=ttyS0,9600n8 root=/dev/ram rw"
+#define CONFIG_BOOTFILE   "flash-galaxy100"
 
 /*
  * Serial configuration
  */
-#define CONFIG_SYS_NS16550_COM1 AST_UART3_BASE
+#define CONFIG_SYS_NS16550_COM1 AST_UART0_BASE
 #define CONFIG_CONS_INDEX       1
-#define CONFIG_ASPEED_COM       AST_UART3_BASE
+#define CONFIG_ASPEED_COM       AST_UART0_BASE
 #define CONFIG_BAUDRATE         9600
 
 /*
@@ -46,4 +46,4 @@
 #include "facebook_common.h"
 #include "ast2500_common.h"
 
-#endif  /* __FBWEDGE100_CONFIG_H */
+#endif  /* __FBBACKPACK_CONFIG_H */
--- include/configs/fbwedge.h
+++ include/configs/fbwedge.h
@@ -8,38 +8,42 @@
 #ifndef __FBWEDGE_CONFIG_H
 #define __FBWEDGE_CONFIG_H
 
-#define CONFIG_BOOTARGS		"debug console=ttyS0,9600n8 root=/dev/ram rw"
-#define CONFIG_UPDATE		"tftp 40800000 ast2400.scr; so 40800000'"
-#define CONFIG_BOOTCOMMAND 	"bootm 20080000"	/* Location of FIT */
-#define CONFIG_BOOTFILE		"all.bin"
+#define CONFIG_IDENT_STRING "\nFacebook Wedge OpenBMC"
+#define CONFIG_FBWEDGE 1
 
-/*
- * Memory Configuration
- * Deprecating with v2016.03: CONFIG_ASPEED_WRITE_DEFAULT_ENV
- */
-#define PHYS_SDRAM_1_SIZE	0x10000000 	/* 256 MB */
+#define CONFIG_SYS_LONGHELP
+
+#define CONFIG_BOOTARGS   "debug console=ttyS2,9600n8 root=/dev/ram rw"
+#define CONFIG_BOOTFILE   "flash-wedge"
 
 /*
  * Serial configuration
  */
-#define CONFIG_CONS_INDEX	3
-#define CONFIG_BAUDRATE		9600
-#define CONFIG_ASPEED_COM	0x1e78e000	/* COM3 */
-#define CONFIG_SYS_LOADS_BAUD_CHANGE
+#define CONFIG_SYS_NS16550_COM1 AST_UART3_BASE
+#define CONFIG_CONS_INDEX       1
+#define CONFIG_ASPEED_COM       AST_UART3_BASE
+#define CONFIG_BAUDRATE         9600
+
+/*
+ * UART configurtion
+ */
+#define CONFIG_ASPEED_UART1_ENABLE
+#define CONFIG_ASPEED_UART1_BMC // by default, src is LPC for UART1
+#define CONFIG_ASPEED_UART2_ENABLE
+#define CONFIG_ASPEED_UART2_BMC // by default, src is LPC for UART2
+#define CONFIG_ASPEED_UART3_ENABLE
+#define CONFIG_ASPEED_UART4_ENABLE
 
 /*
  * NIC configuration
  */
 #define CONFIG_MAC2_ENABLE
-#define CONFIG_MAC1_PHY_SETTING		0
-#define CONFIG_MAC2_PHY_SETTING		0
-#define CONFIG_ASPEED_MAC_NUMBER	2
-#define CONFIG_ASPEED_MAC_CONFIG	2
-
-#define CONFIG_MAC1_PHY_LINK_INTERRUPT
-#define CONFIG_MAC2_PHY_LINK_INTERRUPT
+#define CONFIG_MAC1_PHY_SETTING     0
+#define CONFIG_MAC2_PHY_SETTING     0
+#define CONFIG_ASPEED_MAC_NUMBER    2
+#define CONFIG_ASPEED_MAC_CONFIG    2
 
 #include "facebook_common.h"
-#include "ast2400_common.h"
+#include "ast2500_common.h"
 
 #endif  /* __FBWEDGE_CONFIG_H */

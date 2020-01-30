fe58702641dc4a42488d432d669515b03a6be3fd
Dean Kalla <deank@arista.com>
2020-01-29 22:59:16 -0800

    Add meta-elbert directory initial commit. (#2)
    Summary:
    There are many ELBERTTODO in place as placeholders for areas that will
    need revisiting when the time comes.
    
    elbert at this point is still using AST2520, NCSI, etc.
    
    Testing Done:
    Compiled U-boot and kernel. Flashed combined image on device and things
    booted up okay.
    
    Test Log:
    
    Bootup log:
    Starting mTerm console server...done.
    ELBERT POWER_ON script not implemented yet!
    mTerm_log: mTerm: daemon started
    Starting USB Serial Console: usbcons.
    Remove poweroff command from .bash
    INIT: Entering runlevel: 5
    [   52.354481] using random self ethernet address
    [   52.407741] using random host ethernet address
    [   52.460951] using host ethernet address: 02:00:00:00:00:02
    Configuring network interfaces... [   52.460966] using self ethernet address: 02:00:00:00:00:01
    [   52.724935] usb0: HOST MAC 02:00:00:00:00:02
    [   52.841967] usb0: MAC 02:00:00:00:00:01
    [   53.074171] g_cdc gadget: CDC Composite Gadget, version: King Kamehameha Day 2008
    [   53.163847] g_cdc gadget: g_cdc ready
    [   53.337137] 8021q: adding VLAN 0 to HW filter on device eth0
    [   53.408785] ftgmac100 1e660000.ethernet eth0: NCSI: Handler for packet type 0x82 returned -19
    net.ipv6.conf.usb0.autoconf = 0
    net.ipv6.conf.usb0.router_solicitations = 0
    done.
    Setup dhclient for IPv6... done.
    reloading rsyslogd ... done
    Starting OpenBSD Secure Shell server: sshd
    done.
    Starting domain name service: namedwrote key file "/etc/bind/rndc.key"
    .
    Starting ntpd: done
    starting rsyslogd ... done
    ok: run: restapi: (pid 1530) 0s
    Setup crond...starting Busybox Periodic Command Scheduler: crond... done.
    done.
    
    OpenBMC Release elbert-5b6aad19a-dirty
    
    bmc-oob. login:
    
    # Version (dirty build)
    root@bmc-oob:~# cat /etc/issue
    OpenBMC Release elbert-5b6aad19a-dirty
    Pull Request resolved: https://github.com/facebookexternal/openbmc.arista/pull/2
    
    Differential Revision: D19525126
    
    Pulled By: joancaneus
    
    fbshipit-source-id: 5b3c186761

---
+++ include/configs/fbelbert.h
@@ -0,0 +1,64 @@
+/*
+ * (C) Copyright 2004-Present
+ * Teddy Reed <reed@fb.com>, Facebook, Inc.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef __FBELBERT_CONFIG_H
+#define __FBELBERT_CONFIG_H
+
+#define CONFIG_FBELBERT 1
+
+#define CONFIG_SYS_LONGHELP
+
+#define CONFIG_BOOTARGS   "debug console=ttyS0,9600n8 root=/dev/ram rw"
+#define CONFIG_BOOTFILE   "flash-elbert"
+
+/*
+ * Serial configuration
+ */
+#define CONFIG_SYS_NS16550_COM1 AST_UART0_BASE
+#define CONFIG_CONS_INDEX       1
+#define CONFIG_ASPEED_COM       AST_UART0_BASE
+#define CONFIG_BAUDRATE         9600
+
+/*
+ * UART configurtion
+ */
+#define CONFIG_ASPEED_UART1_ENABLE
+#define CONFIG_ASPEED_UART1_BMC // by default, src is LPC for UART1
+#define CONFIG_ASPEED_UART2_ENABLE
+#define CONFIG_ASPEED_UART2_BMC // by default, src is LPC for UART2
+#define CONFIG_ASPEED_UART3_ENABLE
+#define CONFIG_ASPEED_UART4_ENABLE
+#define CONFIG_ASPEED_UART5_MAP_IO6
+
+/*
+ * NIC configuration
+ */
+/*ELBERTTODO 447405 Review MAC ALLOCATION */
+#define CONFIG_MAC2_ENABLE
+#define CONFIG_MAC1_PHY_SETTING     2
+#define CONFIG_MAC2_PHY_SETTING     0
+#define CONFIG_ASPEED_MAC_NUMBER  1
+#define CONFIG_ASPEED_MAC_CONFIG  1
+
+#define CONFIG_MII_
+#define CONFIG_CMD_MII
+
+/*
+ * Enable DRAM ECC, going to lose 1/8 of memory
+ */
+#define CONFIG_DRAM_ECC
+
+/*
+ * Watchdog configuration, needed for TPM hardware reset
+ * This will trigger GPIOAB2 output high when the CPU is reset
+ */
+#define CONFIG_ASPEED_WATCHDOG_TRIGGER_GPIO
+
+#include "facebook_common.h"
+#include "ast2500_common.h"
+
+#endif  /* __FBELBERT_CONFIG_H */

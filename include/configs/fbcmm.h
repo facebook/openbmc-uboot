020de6e6bbf22eda69be05f52c7cd648335e60f7
Dan Zhang <zhdaniel@fb.com>
2020-03-24 18:00:51 -0700

    u-boot v2019.04: support cmm
    Summary: Add support for CMM
    
    Test Plan: Build and boot on CMM
    
    Reviewers: tfang, taoren, #openbmc
    
    Reviewed By: taoren
    
    Differential Revision: https://phabricator.intern.facebook.com/D20537828
    
    Signature: 20537828:1585000421:3567d29ee58f4d47bc6589cb9331708cbbc20685

---
+++ include/configs/fbcmm.h
@@ -0,0 +1,56 @@
+/*
+ * (C) Copyright 2004-Present
+ * Teddy Reed <reed@fb.com>, Facebook, Inc.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef __FBCMM_CONFIG_H
+#define __FBCMM_CONFIG_H
+
+#define CONFIG_FBCMM 1
+
+#ifndef CONFIG_SYS_LONGHELP
+#define CONFIG_SYS_LONGHELP
+#endif
+
+#define CONFIG_BOOTARGS   "debug console=ttyS1,9600n8 root=/dev/ram rw"
+#define CONFIG_BOOTFILE   "flash-cmm"
+
+/*
+ * Serial configuration
+ */
+#define CONFIG_SYS_NS16550_COM1 AST_UART1_BASE
+#define CONFIG_CONS_INDEX       1
+#define CONFIG_ASPEED_COM       AST_UART1_BASE
+
+/*
+ * UART configurtion
+ */
+#define CONFIG_ASPEED_UART1_ENABLE
+#define CONFIG_ASPEED_UART1_BMC // by default, src is LPC for UART1
+#define CONFIG_ASPEED_UART2_ENABLE
+#define CONFIG_ASPEED_UART2_BMC // by default, src is LPC for UART2
+#define CONFIG_ASPEED_UART3_ENABLE
+#define CONFIG_ASPEED_UART4_ENABLE
+#define CONFIG_ASPEED_UART5_MAP_IO6
+
+/*
+ * NIC configuration
+ */
+#define CONFIG_MAC2_ENABLE
+#define CONFIG_MAC1_PHY_SETTING     0
+#define CONFIG_MAC2_PHY_SETTING     0
+#define CONFIG_ASPEED_MAC_NUMBER  2
+#define CONFIG_ASPEED_MAC_CONFIG  2
+
+
+/*
+ * Enable DRAM ECC, going to lose 1/8 of memory
+ */
+#define CONFIG_DRAM_ECC
+
+#include "facebook_common.h"
+#include "ast2500_common.h"
+
+#endif  /* __FBCMM_CONFIG_H */

617c17f8b1afa921d8a3e775f4d00c77a02b594f
Dan Zhang <zhdaniel@fb.com>
2020-03-24 21:22:29 -0700

    u-boot v2019.04: Support fbal
    Summary: Add support of FBAL
    
    Test Plan: Test and boot with QEMU
    
    Reviewers: tfang, amithash, taoren, #openbmc
    
    Reviewed By: taoren
    
    Differential Revision: https://phabricator.intern.facebook.com/D20538805
    
    Signature: 20538805:1585000447:646083f3260f78476dff4d5125506fee1d5a54ab
    
    fbshipit-source-id: 689f59f56bc

---
+++ include/configs/fbal.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2012-2020  ASPEED Technology Inc.
+ * Ryan Chen <ryan_chen@aspeedtech.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __FBAL_CONFIG_H
+#define __FBAL_CONFIG_H
+
+#define CONFIG_FBAL 1
+
+#define CONFIG_BOOTARGS          "debug console=ttyS0,57600n8 root=/dev/ram rw printk.time=1 dual_flash=2"
+#define CONFIG_BOOTFILE          "flash-angelslanding"
+/*
+ * Serial configuration
+ */
+#define CONFIG_SYS_NS16550_MEM32
+#define CONFIG_SYS_NS16550_REG_SIZE     -4
+#define CONFIG_SYS_NS16550_COM1         AST_UART0_BASE
+#define CONFIG_CONS_INDEX               1
+#define CONFIG_BAUDRATE                 57600
+
+/*
+ * NIC configuration
+ */
+#define CONFIG_MAC1_PHY_SETTING     2
+#define CONFIG_MAC2_PHY_SETTING     0
+#define CONFIG_ASPEED_MAC_NUMBER  1
+#define CONFIG_ASPEED_MAC_CONFIG  1 /* config MAC1 */
+
+
+/*
+ * Enable DRAM ECC, going to lose 1/8 of memory
+ */
+#define CONFIG_DRAM_ECC
+
+/*
+ * Watchdog configuration, needed for TPM hardware reset
+ * This will trigger GPIOAB2 output high when the CPU is reset
+ */
+#define CONFIG_ASPEED_WATCHDOG_TRIGGER_GPIO
+
+/*
+ * Enable 2x I/O read command
+ */
+#define CONFIG_FLASH_SPIx2_Dummy
+
+#include "facebook_common.h"
+#include "ast2500_common.h"
+
+#endif  /* __FBAL_CONFIG_H */

f1f3c7adf0ea71382c55cf43bb7eae4b25b4de64
Dan Zhang <zhdaniel@fb.com>
2020-06-22 16:22:51 -0700

    GrandCanyon: Create the GrandCanyon OpenBMC build
    Reviewed By: benwei13
    
    Differential Revision: D22156691
    
    fbshipit-source-id: 89668bddce

---
+++ include/configs/facebook_grandcanyon.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2020 Facebook Inc.
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <configs/aspeed-common.h>
+
+#define CONFIG_SYS_MEMTEST_START	(CONFIG_SYS_SDRAM_BASE + 0x300000)
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x5000000)
+
+#define CONFIG_SYS_UBOOT_BASE		CONFIG_SYS_TEXT_BASE
+
+/* Memory Info */
+#define CONFIG_SYS_LOAD_ADDR		0x83000000
+
+
+#endif	/* __CONFIG_H */

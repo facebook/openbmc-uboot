03c08fc0188ee179c44eaeb2907e46ab11d65327
Tao Ren <taoren@fb.com>
2020-08-17 10:51:28 -0700

    u-boot-v2019.04: cmm: fixup config files
    Reviewed By: zhdaniel12
    
    Differential Revision: D23176253
    
    fbshipit-source-id: a0b7ec4ecc

--- include/configs/fbcmm.h
+++ include/configs/fbcmm.h
@@ -14,7 +14,7 @@
 #define CONFIG_SYS_LONGHELP
 #endif
 
-#define CONFIG_BOOTARGS   "debug console=ttyS1,9600n8 root=/dev/ram rw"
+#define CONFIG_BOOTARGS   "console=ttyS1,9600n8 root=/dev/ram rw"
 #define CONFIG_BOOTFILE   "flash-cmm"
 
 /*

d7d5ad83daba59d1bf00373ef5cd13b81c8f48a0
RickyWu-wiwynn <ricky_cx_wu@wiwynn.com>
2020-09-08 11:32:59 -0700

    uboot v2019.04: use SOC reset in grandcanyon (#1195)
    Summary:
    1. Define AST_SYSRESET_WITH_SOC to make sysreset use SOC reset.
    2. Use AST_SYS_RESET_WITH_SOC as SOC reset mask 1.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/1195
    
    Test Plan:
    1. Build and test PASS on AST2600-A1 EVB.
    2. Test 'reset' in uboot.
    
    fbgc# reset
    resetting ...
    ?)?
    
    U-Boot 2019.04 grandcanyon-3681d933c8a-dirty (Sep 07 2020 - 10:15:49
    +0000)
    
    SOC: AST2600-A1
    RST: WDT1 SOC
    2nd Boot: Enable, Dual SPI : Primary
    SPI: 3/4 byte mode auto detection
    eSPI Mode: SIO:Enable : SuperIO-2e
    Eth: MAC0: RGMII, MAC1: RGMII, MAC2: RGMII, MAC3: RGMII
    Model: Aspeed BMC
    DRAM:  already initialized, 1008 MiB
    MMC:   emmc_slot0@100: 0
    Loading Environment from SPI Flash... SF: Detected w25q512jv with page
    size 256 Bytes, erase size 4 KiB, total 64 MiB
    *** Warning - bad CRC, using default environment
    
    In:    serial
    Out:   serial
    Err:   serial
    Model: Aspeed BMC
    Net:
    Warning: ftgmac@1e680000 (eth1) using random MAC address -
    92:59:78:2d:ed:39
    eth1: ftgmac@1e680000
    Hit any key to stop autoboot:  0
    
    3. Check the value of WDT1C and WDT20 before and after reset.
    
    *Before reset
    
    fbgc# md 0x1e78501c
    1e78501c: 030f1ff1 03fffff1 00000000 00000000    ................
    
    fbgc# md 0x1e785020
    1e785020: 03fffff1 00000000 00000000 00000000    ................
    
    *After reset
    
    fbgc# md 0x1e78501c
    1e78501c: 030f1ff1 03fffff1 00000000 00000000    ................
    
    fbgc# md 0x1e785020
    1e785020: 03fffff1 00000000 00000000 00000000    ................
    
    Reviewed By: benwei13
    
    Differential Revision: D23566614
    
    Pulled By: zhdaniel12
    
    fbshipit-source-id: 93721e5165

--- include/configs/facebook_grandcanyon.h
+++ include/configs/facebook_grandcanyon.h
@@ -21,6 +21,15 @@
 #undef CONFIG_ENV_OFFSET
 #define CONFIG_ENV_OFFSET	0xE0000
 
-
+/* u-boot reset command and reset() is implemented via sysreset
+ * which will trigger the WDT to do full-chip reset by default.
+ * But WDT Full-chip reset cannot successfully drive the WDTRST_N pin,
+ * so define AST_SYSRESET_WITH_SOC to make sysreset use SOC reset,
+ * and use AST_SYS_RESET_WITH_SOC as SOC reset mask #1.
+ * Notice: For system which loop back the WDTRST_N pin to BMC SRST pin.
+ * the value of AST_SYSRESET_WITH_SOC does not matter, because
+ * no matter how the BMC will get full reset by SRST pin.
+ */
+#define AST_SYSRESET_WITH_SOC 0x030f1ff1
 
 #endif	/* __CONFIG_H */

b0dd5d69fba279421d89af14344633752d457725
wxiaohua1217 <55966046+wxiaohua1217@users.noreply.github.com>
2020-11-10 09:19:43 -0800

    aspeed: uboot: set gpio g6 low to fix kernel can not load bus24 pca9548 driver (#809)
    Summary:
    Background: Now, FB cancelled the PFR function in Minipack2 and there is no LED drove by MAX10 feature any more. The change happened after we gerbered out our DVT system.
    In current DVT design, MAX10 report it’s status first and the LED will drove by CPLD. After BMC boot up, BMC should switch the control path to BMC by pull down the GPIO G6. Since there is no more PFR feature, BMC should switch the control path to BMC by pull down the GPIO G6  at the beginning of system boot up.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/809
    
    Test Plan:
    ```
    [   29.162112] pca954x 2-0070: registered 8 multiplexed busses for I2C switch pca9548
    [   29.254531] i2c i2c-24: Added multiplexed i2c bus 48
    [   29.314256] i2c i2c-24: Added multiplexed i2c bus 49
    [   29.374117] i2c i2c-24: Added multiplexed i2c bus 50
    [   29.433828] i2c i2c-24: Added multiplexed i2c bus 51
    [   29.493498] i2c i2c-24: Added multiplexed i2c bus 52
    [   29.553186] i2c i2c-24: Added multiplexed i2c bus 53
    [   29.612850] i2c i2c-24: Added multiplexed i2c bus 54
    [   29.672533] i2c i2c-24: Added multiplexed i2c bus 55
    [   29.731960] pca954x 24-0071: registered 8 multiplexed busses for I2C switch pca9548
    [   29.823690] i2c i2c-8: Added multiplexed i2c bus 24
    [   29.883498] i2c i2c-25: Added multiplexed i2c bus 56
    [   29.943206] i2c i2c-25: Added multiplexed i2c bus 57
    [   30.002890] i2c i2c-25: Added multiplexed i2c bus 58
    [   30.062573] i2c i2c-25: Added multiplexed i2c bus 59
    [   30.122351] i2c i2c-25: Added multiplexed i2c bus 60
    [   30.182083] i2c i2c-25: Added multiplexed i2c bus 61
    [   30.241758] i2c i2c-25: Added multiplexed i2c bus 62
    [   30.301451] i2c i2c-25: Added multiplexed i2c bus 63
    ```
    
    Reviewed By: mikechoifb
    
    Differential Revision: D24862164
    
    Pulled By: tao-ren
    
    fbshipit-source-id: e2481ac44d

--- board/aspeed/facebook_fuji/facebook_fuji.c
+++ board/aspeed/facebook_fuji/facebook_fuji.c
@@ -4,6 +4,8 @@
  */
 
 #include <common.h>
+#include <asm/io.h>
+
 /* SCU Base Address */
 #define AST_SCU_VA_BASE             0x1E6E2000
 /* FMC Base Address */
@@ -56,10 +58,45 @@ void Enable_ABR_BOOT(void)
     *((volatile unsigned long *)(WDT_CONTROL_STATUS_REG)) = (WDT_ENABLE);
 }
 
+void set_gpio_g6_output_low(void)
+{
+#define GPIO_G		16
+#define GPIO_G6_MASK  	BIT(GPIO_G + 6)
+	/* GPIO020: GPIO_E/F/G/H Data Value Register
+	 * 31:24 Port GPIOH[7:0] data register
+	 * 23:16 Port GPIOG[7:0] data register
+	 * 15:8 Port GPIOF[7:0] data register
+	 * 7:0 Port GPIOE[7:0] data register
+	 */
+	u32 value = readl(0x1e780020);
+	/* GPIO024: GPIO_E/F/G/H Direction Value Register
+	 * 31:24 Port GPIOH[7:0] direction control
+	 * 0: Select input mode 1: Select output mode
+	 * 23:16 Port GPIOG[7:0] direction control
+	 * 0: Select input mode 1: Select output mode
+	 * 15:8 Port GPIOF[7:0] direction control
+	 * 0: Select input mode 1: Select output mode
+	 * 7:0 Port GPIOE[7:0] direction control
+	 */
+	u32 direction = readl(0x1e780024);
+
+	direction |= GPIO_G6_MASK;
+	value &= ~GPIO_G6_MASK;
+	writel(direction, 0x1e780024);
+	writel(value, 0x1e780020);
+}
+
 int board_init(void)
 {
 #if CONFIG_IS_ENABLED(ABR_BOOT)
 	Enable_ABR_BOOT();
 #endif
+	/* Because PFR led feature requirement, In DVT Design MAX10 report it’s
+	 * status first and the LED will drove by CPLD. This cause BMC bound the
+	 * PCA9548 driver fail during loading Kernel.
+	 * To fix this issue, after BMC boot up, BMC should switch the control
+	 * path to BMC by pull down the GPIO G6.
+	 */
+	set_gpio_g6_output_low();
 	return 0;
 }

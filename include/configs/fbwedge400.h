8be747e39a4828ce03ffac3532d3d0a7f06d95af
Tianhui Xu <tianhui@celestica.com>
2021-02-03 09:50:23 -0800

    u-boot-v2019.04: support Facebook Wedge400 BMC (#968)
    Summary:
    support Facebook Wedge400 BMC
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/968
    
    Test Plan:
    1) "bitbake minipack-image" succeeded.
    2) BMC can tftpboot from u-boot console.
    3) booted up wedge400 image successfully.
    
    Reviewed By: benwei13
    
    Differential Revision: D26227199
    
    Pulled By: tao-ren
    
    fbshipit-source-id: 5401931088

--- include/configs/fbwedge400.h
+++ include/configs/fbwedge400.h
@@ -30,6 +30,7 @@
 #define CONFIG_SYS_NS16550_COM1 AST_UART1_BASE
 #define CONFIG_CONS_INDEX       1
 #define CONFIG_ASPEED_COM       AST_UART1_BASE
+
 /*
  * UART configurtion
  */
@@ -39,29 +40,20 @@
 #define CONFIG_ASPEED_UART2_BMC // by default, src is LPC for UART2
 #define CONFIG_ASPEED_UART3_ENABLE
 #define CONFIG_ASPEED_UART4_ENABLE
+#define CONFIG_ASPEED_UART5_MAP_IO6
+
 /*
  * NIC configuration
  */
+#define CONFIG_PHY_BROADCOM_MODE1000X
+/*
 #define CONFIG_MAC2_ENABLE
 #define CONFIG_MAC1_PHY_SETTING     0
 #define CONFIG_MAC2_PHY_SETTING     0
 #define CONFIG_MAC2_PHY_LINK_INTERRUPT
 #define CONFIG_ASPEED_MAC_NUMBER    2
 #define CONFIG_ASPEED_MAC_CONFIG    2
-
-/*
- * Watchdog configuration
- */
-#define CONFIG_ASPEED_ENABLE_WATCHDOG
-#define CONFIG_ASPEED_WATCHDOG_2ND_BOOT
-
-// #define CONFIG_MII_
-// #define CONFIG_CMD_MII
-
-/*
- * SPI flash configuration
- */
-#define CONFIG_2SPIFLASH
+*/
 
 #include "facebook_common.h"
 #include "ast2500_common.h"

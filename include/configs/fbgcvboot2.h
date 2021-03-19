f7240a4b1f60b1f99c2bf434cae20d7f2e7b132c
Dan Zhang <zhdaniel@fb.com>
2021-03-19 15:55:58 -0700

    fbgc: Add fbgcvboot2 machine build with vboot
    Reviewed By: benwei13
    
    Differential Revision: D27162169
    
    fbshipit-source-id: d5cbd04efd

---
+++ include/configs/fbgcvboot2.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2020 Facebook Inc.
+ */
+
+#ifndef FBGCVBOOT2__CONFIG_H
+#define FBGCVBOOT2__CONFIG_H
+
+#include <configs/fbobmc-ast-g6.h>
+
+
+#endif	/* FBGCVBOOT2__CONFIG_H */

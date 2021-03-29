21ccb852f9837d2cafc8200761568e12da21a8ca
Dan Zhang <zhdaniel@fb.com>
2021-03-29 15:18:13 -0700

    vboot: add vboot support in ast-g6 u-boot proper
    Reviewed By: benwei13
    
    Differential Revision: D27384025
    
    fbshipit-source-id: 5020cc4c95

---
+++ board/fb-obmc/ast/vboot.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2021 Facebook Inc.
+ */
+
+#include <common.h>
+#include <asm/arch/vbs.h>
+#include "tpm-spl.h"
+
+static void vboot_finish(void)
+{
+	/* Clean the handoff marker from ROM. */
+	volatile struct vbs *vbs = (volatile struct vbs *)AST_SRAM_VBS_BASE;
+	vbs->rom_handoff = 0x0;
+
+#ifdef CONFIG_ASPEED_TPM
+	if (vbs->hardware_enforce || vbs->software_enforce ||
+	    vbs->recovery_boot) {
+		/* Only do measure when verifiy boot is enabled */
+		vboot_uboot_do_measures(vbs);
+	}
+	ast_tpm_finish();
+#endif
+}
+
+char *fit_cert_store(void)
+{
+	volatile struct vbs *vbs = (volatile struct vbs *)AST_SRAM_VBS_BASE;
+	return (char *)(vbs->subordinate_keys);
+}
+
+void arch_preboot_os(void)
+{
+	vboot_finish();
+}

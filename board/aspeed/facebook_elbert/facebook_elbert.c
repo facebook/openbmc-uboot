3cc8546939be89f42ba9147a7c37c20afc2cc217
Geoff Hanson <ghanson@arista.com>
2021-04-30 16:39:34 -0700

    Elbert management interface 1G support (#179)
    Summary:
    Change the fixed-speed to 1000 for both u-boot and the kernel.
    
    For A2 support, specify mac3-clk-delay in both the U-boot and
    kernel dts files. This config only is used in A2 and greater.
    
    Enhance the u-boot logic to program the BCM53134 registers that
    control the link speed. Program them to 1G.
    
    Implement a setup_bcm53134.sh to program the BCM53134 registers
    in the kernel.
    
    With these changes, the default register values in the EEPROM
    are overridden. Therefore, the newer BMC image will work with
    older EEPROM images. However, older BMC images will not work
    with newer EEPROM images.
    
    Also note that if the u-boot image has the newer version,
    but the kernel image is the older image, then the management
    interface will not work. Therefore, the u-boot image should
    not be programmed independently.
    
    Testing performed:
    1. Broke into u-boot and verified the management link worked:
    ELBERT-boot=> setenv ipaddr 172.24.146.143
    ELBERT-boot=> ping 172.24.146.129
    ftgmac@1e690000: link up, 1000 Mbps full-duplex mac:c2:06:e4:8c:b3:b4
    Using ftgmac@1e690000 device
    host 172.24.146.129 is alive
    
    2. Booted to the kernel. Inspected the /var/log/boot file
    and verified the BCM53134 registers were not re-programmed.
    Verified ssh to the BMC worked.
    
    3. Programmed the u-boot image to be the existing 100M version
    and the kernel image to be the new 1000M.
    
    Booted to the kernel. Observed the following message in /var/log/boot:
    BCM53134 registers not as expected. Configuring now.
    Re-program the BCM53134 EEPROM to fix this permanently.
    
    Verified ssh to the BMC worked.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.arista/pull/179
    
    Reviewed By: binhuang00
    
    Differential Revision: D28014076
    
    Pulled By: joancaneus
    
    fbshipit-source-id: 3f416bdb38

--- board/aspeed/facebook_elbert/facebook_elbert.c
+++ board/aspeed/facebook_elbert/facebook_elbert.c
@@ -10,7 +10,12 @@
 #define AST_SCU_VA_BASE             0x1E6E2000
 /* FMC Base Address */
 #define AST_FMC_VA_BASE             0x1E620000
+/* PMI Base Address */
+#define AST_PMI_VA_BASE             0x1E650000
 
+#define SCU_RESET_SET2_REG          (AST_SCU_VA_BASE + 0x50)
+#define SCU_RESET_CLEAR2_REG        (AST_SCU_VA_BASE + 0x54)
+#define SCU_PIN_CONTROL8_REG        (AST_SCU_VA_BASE + 0x430)
 #define SCU_HW_STRAPPING_REG        (AST_SCU_VA_BASE + 0x510)
 #define SCU_HW_CLEAR_STRAPPING_REG  (AST_SCU_VA_BASE + 0x514)
 
@@ -19,12 +24,22 @@
 #define WDT_TIMER_RELOAD_REG        (AST_FMC_VA_BASE + 0x68)
 #define WDT_CNT_RESTART_REG         (AST_FMC_VA_BASE + 0x6C)
 
+#define PMI_MDC_MDIO_CONTROL_REG    (AST_PMI_VA_BASE + 0x00)
+#define PMI_MDC_MDIO_READ_DATA_REG  (AST_PMI_VA_BASE + 0x04)
+
 /* FMC[64] */
 #define WDT_DISABLE                 0x00                /* Disable WDT2 */
 #define WDT_ENABLE                  0x01                /* Enable WDT2 */
 #define WDT_CLEAR_EVNET_COUNTER     0xEA000000          /* Clear WDT2 event counter */
 #define WDT_RESTART_TIMER_REG       0xFFFF4755          /* Restart WDT2 timer register */
 #define WDT_TIMEOUT                 0xBB8              	/* 5 Minutes (3000 * 0.1sec) */
+
+/* SCU[50] */
+#define SCU_RESET_MII_CONTROLLER    (1 << 3)            /* Reset the MII controller */
+
+/* SCU[430] */
+#define SCU_ENABLE_MDC_PIN          (1 << 16)           /* Enable MDC */
+#define SCU_ENABLE_MDIO_PIN         (1 << 17)           /* Enable MDIO */
 
 /* SCU[510] */
 #define SCU_ENABLE_ABR              (1 << 11)           /* Enable ABR boot */
@@ -34,6 +49,11 @@
 /* FMC[50] */
 #define WAIT_SPI_WIP_IDLE           (1 << 1)            /* Enable wait SPI WIP idel */
 #define GENERATE_SOFT_RESET_COMMAND (1 << 0)            /* Enable generate soft-reset command */
+
+/* MDIO OOB */
+#define MDIO_OOB_PAGE_REG 16
+#define MDIO_OOB_ADDR_REG 17
+#define MDIO_OOB_DATA_REG 24
 
 void Enable_ABR_BOOT(void)
 {
@@ -58,10 +78,153 @@ void Enable_ABR_BOOT(void)
     *((volatile unsigned long *)(WDT_CONTROL_STATUS_REG)) = (WDT_ENABLE);
 }
 
+
+#define MDIO_UDELAY               10
+#define OOB_CONTROL_PAGE          0x0
+#define OOB_IMP_PORT_STATE_REG    0xe
+#define OOB_IMP_RGMII_CONTROL_REG 0x60
+
+void mdio_init(void)
+{
+    unsigned long val;
+    *(volatile unsigned long *)(SCU_RESET_CLEAR2_REG) = SCU_RESET_MII_CONTROLLER;
+
+    val = *(volatile unsigned long *)(SCU_PIN_CONTROL8_REG);
+    val |= (SCU_ENABLE_MDC_PIN | SCU_ENABLE_MDIO_PIN);
+    *(volatile unsigned long *)(SCU_PIN_CONTROL8_REG) = val;
+}
+
+void mdio_restore(void)
+{
+    unsigned long val;
+    *(volatile unsigned long *)(SCU_RESET_SET2_REG) = SCU_RESET_MII_CONTROLLER;
+
+    val = *(volatile unsigned long *)(SCU_PIN_CONTROL8_REG);
+    val &= ~(SCU_ENABLE_MDC_PIN | SCU_ENABLE_MDIO_PIN);
+    *(volatile unsigned long *)(SCU_PIN_CONTROL8_REG) = val;
+}
+
+int mdio_wait_op_complete(unsigned int reg_addr, unsigned long mask,
+                          unsigned long wait_val)
+{
+    int maxTries = 30;
+    int i;
+    unsigned long val;
+    for (i = 0; i < maxTries; ++i) {
+        val = *(volatile unsigned long *)reg_addr;
+        if ((val & mask) == wait_val) {
+            return 0;
+        }
+        udelay(MDIO_UDELAY);
+    }
+    return -1;
+}
+
+/* mdio_read returns register values between 0x0 and 0xffff
+   in the case of success and -1 in the case of failure. */
+int mdio_read(unsigned long addr)
+{
+    unsigned long reg_val;
+    unsigned long fire_busy = 1 << 31;
+    unsigned long ctrl_idle = 1 << 16;
+
+    reg_val = fire_busy
+            | (1 << 28)    /* clause_22 */
+            | (1 << 27)    /* read request */
+            | (0x1e << 21) /* pseudo-phy port address */
+            | (addr << 16);
+    *(volatile unsigned long *)(PMI_MDC_MDIO_CONTROL_REG) = reg_val;
+    if (mdio_wait_op_complete(PMI_MDC_MDIO_READ_DATA_REG, ctrl_idle,
+                              ctrl_idle)) {
+        return -1;
+    }
+    reg_val = *(volatile unsigned long *)(PMI_MDC_MDIO_READ_DATA_REG);
+    return (int)(reg_val & 0xffff);
+}
+
+int mdio_write(unsigned long addr, unsigned long val)
+{
+    unsigned long reg_val;
+    unsigned long fire_busy = 1 << 31;
+
+    reg_val = fire_busy
+            | (1 << 28)    /* clause_22 */
+            | (1 << 26)    /* write request */
+            | (0x1e << 21) /* pseudo-phy port address */
+            | (addr << 16)
+            | val;
+    *(volatile unsigned long *)(PMI_MDC_MDIO_CONTROL_REG) = reg_val;
+    return mdio_wait_op_complete(PMI_MDC_MDIO_CONTROL_REG, fire_busy, 0);
+}
+
+int reg_wait_addr_reg_acked(void)
+{
+    int maxTries = 30;
+    int i;
+    int response;
+    unsigned long op;
+    for (i = 0; i < maxTries; ++i) {
+        response = mdio_read(MDIO_OOB_ADDR_REG);
+        if (response < 0) {
+            return -1;
+        }
+        op = ((unsigned long)response) & 0x3;
+        if (!op) {
+            return 0;
+        }
+        udelay(MDIO_UDELAY);
+    }
+    return -1;
+}
+
+int program_oob_register(unsigned long page, unsigned long addr, unsigned long val)
+{
+    unsigned long data;
+
+    data = (page << 8) | 0x1 /* access enable */;
+    if (mdio_write(MDIO_OOB_PAGE_REG, data)) {
+        return -1;
+    }
+    if (mdio_write(MDIO_OOB_DATA_REG, val)) {
+        return -1;
+    }
+    data = (addr << 8) | 0x1 /* write op */;
+    if (mdio_write(MDIO_OOB_ADDR_REG, data)) {
+        return -1;
+    }
+
+    if (reg_wait_addr_reg_acked()) {
+        return -1;
+    }
+
+    data = 0x0 /* access disable */;
+    if (mdio_write(MDIO_OOB_PAGE_REG, data)) {
+        return -1;
+    }
+
+    return 0;
+}
+
+/* Configure the BCM53134 registers to 1G */
+void configureBcm53134(void)
+{
+    /* Initialize the MDIO controller for programming */
+    mdio_init();
+
+    /* These values set the speed to 1G, overriding the default settings in the
+       EEPROM */
+    program_oob_register(OOB_CONTROL_PAGE, OOB_IMP_PORT_STATE_REG, 0x8b);
+    program_oob_register(OOB_CONTROL_PAGE, OOB_IMP_RGMII_CONTROL_REG, 0x0);
+
+    /* Restore the MDIO controller setting */
+    mdio_restore();
+}
+
 int board_init(void)
 {
 #if CONFIG_IS_ENABLED(ABR_BOOT)
 	Enable_ABR_BOOT();
 #endif
+   configureBcm53134();
 	return 0;
 }

8b93ef2a9690ee4610b4a8c47a03ac5240bf6a2a
peteryin <peter.yin@quantatw.com>
2022-02-23 10:34:04 -0800

    GT: Add ESPI in uboot (#2816)
    Summary:
    Add espi reg config and 80 port config
    
    TestPlan:
    Kernel:5.10
    SOC:ast2600
    Platform:GrandTeton
    
    [1D][19][A1][A3][A3][A7][A9][A9][A2][A2][A2][A2][A2][AA][AF][E0][E0][B0][B5][7E][CF]
    [7E][73][CD][B0][7E][C1][70][B1][B1][7E][C2][7E][70][7E][7E][AF][AF][1D][19][A1][A3]
    [A3][A7][A9][A9][A2][A2][A7][A7][A7][A9][A9][A8][AA][AE][E0][E0][E0][E1][E4][E3][E5]
    [AF][AF][B0][B5][7E][CF][7E][73][CD][B0][7E][C1][70][B1][B1][7E][C2][7E][70][7E][7E]
    [B1][B1][B1][B6][7E][B4][7E][B8][C5][B2][C6][B3][B3][B6][B6][B6][B0][B7][B6][B6][7E]
    [7E][7E][B7][B7][B6][B6][B7][B7][7E][70][7E][70][70][B7][7E][BE][BE][7E][7E][D2][7E]
    [D2][D6][70][B9][B7][B7][B7][B8][B8][B8][B8][B8][B8][B8][B8][B8][D7][C9][DA][D9][DB]
    [BA][B9][70][70][70][7E][CB][BB][BB][BB][BB][BB][BB][BB][BB][BB][BB][7E][7E][D0][7E]
    [D0][7E][D0][7E][D1][7E][D1][7E][70][7E][B7][CA][CA][DC][7E][CC][BC][BC][BC][BC][BC]
    [CE][C6][7E][BF][AF][AF][AF][E6][E7][E9][EB][EC][ED][EE]
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/2816
    
    Reviewed By: garnermic
    
    Differential Revision: D34156086
    
    Pulled By: amithash
    
    fbshipit-source-id: 454d0bb182

--- board/fb-obmc/ast/ast-g6.c
+++ board/fb-obmc/ast/ast-g6.c
@@ -10,8 +10,112 @@
 #include <linux/err.h>
 #include <dm/uclass.h>
 #include "util.h"
+#include "ast-g6.h"
 
 DECLARE_GLOBAL_DATA_PTR;
+
+void el_port80_init(void){
+
+  uint32_t value = 0;
+
+  //GPION Settings
+  value  = readl(AST6_GPIO_BASE | 0x7C);
+  value |= 0x0000FF00;  //GPIO N Output
+  writel(value,  AST6_GPIO_BASE | 0x7C);
+
+  //GPIOM/N/O/P Command Source 0
+  value  = readl(AST6_GPIO_BASE | 0x0E0);
+  value |= 1<<8;    //Port GPION[7:0] Command Source 0 = 1 (LPC)
+  writel(value,  AST6_GPIO_BASE | 0x0E0);
+
+  //GPIOM/N/O/P Command Source 1
+  value  = readl(AST6_GPIO_BASE | 0x0E4);
+  value &= ~(1<<8); //Port GPION[7:0] Command Source 1 = 0 (LPC)
+  writel(value,  AST6_GPIO_BASE | 0x0E4);
+
+  //SNPWADR(0x90): LPC Snoop Address Register
+  value  = readl(AST6_LPC_BASE | LPC_SNPWADR);
+  value &= 0xFFFF0000;  //BIT[15:0] - clear;
+  value |= 0x00000080;  //BIT[15:0] - set;
+  writel(value,  AST6_LPC_BASE | LPC_SNPWADR);
+
+  //HICR5(0x80): Host Interface Control Register 5
+  value  = readl(AST6_LPC_BASE | LPC_HICR5);
+  value |= 1;       //Enable snooping address #0
+  value &= ~0xA;
+  writel(value,  AST6_LPC_BASE | LPC_HICR5);
+
+  //HICRB(0x100): Host Interface Control Register B
+  value  = readl(AST6_LPC_BASE | LPC_HICRB);
+  value |= 1<<14;//EnSNP0D: Enable ACCEPT response code for snoop #0 commands, defined in HICR5[0], in eSPI mode.
+  writel(value,  AST6_LPC_BASE | LPC_HICRB);
+
+  writel(0, LPC_PCCR0);
+}
+
+
+void el_espi_init(void){
+  uint32_t value=0;
+
+  //SCU514: SCU514: Hardware Strap2 Clear Register (default)
+  value = readl(AST6_SCU_BASE | 0x514);
+  value |= 1<<6; //eSPI Mode
+  writel(value,  AST6_SCU_BASE | 0x514);
+
+  //SCU434: Multi-function Pin Control #9 (default)
+  value  = readl(AST6_SCU_BASE | 0x434);
+  value |= 0xFF<<16;
+  writel(value,  AST6_SCU_BASE | 0x434);
+
+  //SCU454: Multi-function Pin Control #15
+  value  = readl(AST6_SCU_BASE | 0x454);
+  value &= 0x00FFFFFF;  //Clear BIT[31:24]
+  value |= 0xAA000000;  //BIT[31:24] = LAD3ESPID3~0 Driving Strength
+  writel(value,  AST6_SCU_BASE | 0x454);
+
+  //ESPI000: Engine Control
+  value  = readl(AST6_ESPI_BASE);
+  value |= 1<<4;   //OOB Channel Ready.
+  writel(value,  AST6_ESPI_BASE);
+}
+
+void el_superio_decoder(uint8_t addr) {
+  uint32_t value=0;
+  //Super IO Settings
+  //SCU514: Hardware Strap2 'Clear' Register
+  value  = 0;
+  value |= 1<<3;   //Enable LPC to decode SuperIO 0x2E/0x4E address
+  writel(value,  AST6_SCU_BASE | 0x514);
+
+  //Super IO Settings
+  //SCU510: Hardware Strap2 Register
+  //SuperIO configuration address selection (0 = 0x2E(Default) / 1 = 0x4E)
+  if (addr == 0x4E ) {
+    value  = readl(AST6_SCU_BASE | 0x510);
+    value |= 1<<2; 
+    writel(value,  AST6_SCU_BASE | 0x510);
+  }
+}
+
+
+void debugMsg_espi(void){
+
+  uint32_t value = 0;
+
+  //Debug Message
+  printf("\n[S9S] eSPI Settings\n");
+  value  = readl(AST6_SCU_BASE | 0x510);
+  printf("SCU510 = 0x%.8X , eSPI Mode[6](0 = eSPI)\n" , value);
+  value  = readl(AST6_SCU_BASE | 0x434);
+  printf("SCU434 = 0x%.8X , eSPI Pins[23:16]\n" , value);
+  value  = readl(AST6_SCU_BASE | 0x454);
+  printf("SCU454 = 0x%.8X , eSPI Driving Strength[31:24]\n" , value);
+  value  = readl(AST6_ESPI_BASE);
+  printf("ESPI000 = 0x%.8X , OOB Channel Ready[4]\n" , value);
+
+  //end
+  printf("\n");
+}
 
 #ifdef CONFIG_FBGC
 static void system_status_led_init(void)
@@ -104,5 +208,12 @@ int board_init(void)
 #ifdef CONFIG_ELBERVBOOT
 	configureBcm53134();
 #endif /* ELBERT specific */
+
+#ifdef CONFIG_FBGT
+        el_port80_init();
+        el_espi_init();
+        el_superio_decoder(0x4E);
+        debugMsg_espi();
+#endif /* FBGT specific */
 	return 0;
 }

59ccd0bfc99637cf94b1826d83d7b3b20fca6c88
Dan Zhang <zhdaniel@fb.com>
2022-03-28 16:14:57 -0700

    Reformat tpm-spl.h code style and add header duplicate include protection
    Reviewed By: williamspatrick, garnermic
    
    Differential Revision: D34975600
    
    fbshipit-source-id: 4faf25e2c3b200e3c9fa270a4dc162c59ca52b33

--- board/fb-obmc/ast/tpm-spl.h
+++ board/fb-obmc/ast/tpm-spl.h
@@ -3,7 +3,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0+
  */
-
+#ifndef _BOARD_FBOBMC_AST_TPM_SPL_H
+#define _BOARD_FBOBMC_AST_TPM_SPL_H
 #include <common.h>
 
 #include <asm/arch/vbs.h>
@@ -12,33 +13,33 @@
 #define AST_TPM_MAX_YEARS 5
 
 enum ast_tpm_rollback_type {
-  AST_TPM_ROLLBACK_SUBORDINATE = 0,
-  AST_TPM_ROLLBACK_UBOOT = 1,
-  AST_TPM_ROLLBACK_KERNEL = 2,
+	AST_TPM_ROLLBACK_SUBORDINATE = 0,
+	AST_TPM_ROLLBACK_UBOOT = 1,
+	AST_TPM_ROLLBACK_KERNEL = 2,
 };
 
 struct tpm_rollback_t {
-  uint32_t subordinate;
-  uint32_t uboot;
-  uint32_t kernel;
-  uint32_t fallback_subordinate;
-  uint32_t fallback_uboot;
-  uint32_t fallback_kernel;
+	uint32_t subordinate;
+	uint32_t uboot;
+	uint32_t kernel;
+	uint32_t fallback_subordinate;
+	uint32_t fallback_uboot;
+	uint32_t fallback_kernel;
 };
 
 enum ast_tpm_pcrs {
-  AST_TPM_PCR_SPL = 0,
-  AST_TPM_PCR_FIT = 1,
-  AST_TPM_PCR_UBOOT = 2,
-  AST_TPM_PCR_ENV = 3,
-  AST_TPM_PCR_VBS = 5,
-  AST_TPM_PCR_OS = 9,
+	AST_TPM_PCR_SPL = 0,
+	AST_TPM_PCR_FIT = 1,
+	AST_TPM_PCR_UBOOT = 2,
+	AST_TPM_PCR_ENV = 3,
+	AST_TPM_PCR_VBS = 5,
+	AST_TPM_PCR_OS = 9,
 };
 
 enum ast_tpm_state {
-  AST_TPM_STATE_GOOD = 0,
-  AST_TPM_STATE_FAIL = 1,
-  AST_TPM_STATE_INIT = 2, /* not startup and selftest yet */
+	AST_TPM_STATE_GOOD = 0,
+	AST_TPM_STATE_FAIL = 1,
+	AST_TPM_STATE_INIT = 2, /* not startup and selftest yet */
 };
 /**
  * int ast_tpm_provision() - Perform a 1-time provision of the TPM.
@@ -66,7 +67,7 @@ int ast_tpm_owner_provision(struct vbs *vbs);
  *
  * @return a status for tpm_status
  */
-int ast_tpm_extend(uint32_t index, unsigned char* data, uint32_t data_len);
+int ast_tpm_extend(uint32_t index, unsigned char *data, uint32_t data_len);
 
 /**
  * int ast_tpm_try_version() - Try to verify a version is not rolling back.
@@ -78,7 +79,7 @@ int ast_tpm_extend(uint32_t index, unsigned char* data, uint32_t data_len);
  * @return a status for vboot_status
  */
 int ast_tpm_try_version(struct vbs *vbs, uint8_t image, uint32_t version,
-                        bool no_fallback);
+			bool no_fallback);
 
 /**
  * int ast_tpm_finish() - Lock the physical presence.
@@ -102,3 +103,5 @@ int ast_tpm_get_state(void);
  * @return true if the pcr all bits are 0 or 1, otherwise false.
  */
 bool ast_tpm_pcr_is_open(struct vbs *vbs, uint32_t pcrid);
+
+#endif

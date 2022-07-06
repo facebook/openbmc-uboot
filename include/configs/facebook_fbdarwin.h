1e1c8f7efb21731de06eee31fe82ebd3d9fe74c9
Dan Zhang <zhdaniel@fb.com>
2022-07-06 12:40:00 -0700

    use common board configruaton and init code
    Reviewed By: joancaneus
    
    Differential Revision: D37446791
    
    fbshipit-source-id: 550306c2e4810e5d04adc9a441ec4ef7820fc69d

--- include/configs/facebook_fbdarwin.h
+++ include/configs/facebook_fbdarwin.h
@@ -7,26 +7,8 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#include <configs/aspeed-common.h>
-
-#define CONFIG_SYS_MEMTEST_START	(CONFIG_SYS_SDRAM_BASE + 0x300000)
-#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x5000000)
-
-#define CONFIG_SYS_UBOOT_BASE		CONFIG_SYS_TEXT_BASE
-
-/* Memory Info */
-#define CONFIG_SYS_LOAD_ADDR		0x83000000
-
-#undef CONFIG_ENV_SIZE
-#define CONFIG_ENV_SIZE 0x10000
-#undef CONFIG_ENV_OFFSET
-#define CONFIG_ENV_OFFSET 0xE0000
-
-/*
- * Autoboot configuration
- */
-#define CONFIG_AUTOBOOT_PROMPT		"autoboot in %d seconds (stop with 'Delete' key)...\n"
-#define CONFIG_AUTOBOOT_STOP_STR	"\x1b\x5b\x33\x7e"	/* 'Delete', ESC[3~ */
-#define CONFIG_AUTOBOOT_KEYED
+#define CONFIG_FBDARWIN    1
+#define CONFIG_CMD_MEMTEST2 1
+#include <configs/fbobmc-ast-g6.h>
 
 #endif	/* __CONFIG_H */

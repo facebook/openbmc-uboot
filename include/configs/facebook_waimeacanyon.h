729e423eee3815f849c9c41b15041aa505ff0f64
jungle0220 <Ken.Chen@quantatw.com>
2022-07-25 09:55:53 -0700

    uboot-2019.04: Add support for waimeacanyon (#3067)
    Summary:
    As title
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3067
    
    Reviewed By: williamspatrick
    
    Differential Revision: D38095023
    
    Pulled By: zhdaniel12
    
    fbshipit-source-id: b8d2fec328d03c2e444426374ce2c5530683024e

---
+++ include/configs/facebook_waimeacanyon.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2020 Facebook Inc.
+ */
+
+#ifndef __FACEBOOK_WAIMEACANYON_CONFIG_H
+#define __FACEBOOK_WAIMEACANYON_CONFIG_H
+
+#define CONFIG_FBWC    1
+#define CONFIG_CMD_MEMTEST2 1
+#include <configs/fbobmc-ast-g6.h>
+
+#endif	/* __FACEBOOK_WAIMEACANYON_CONFIG_H */

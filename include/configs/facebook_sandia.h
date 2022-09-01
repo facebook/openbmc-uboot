0590f8468900f7ba21e8f2ea755d9cfb53fe5b3d
wenchungw <vincwang@cisco.com>
2022-09-01 10:42:48 -0700

    sandia: yocto: Create machine Sandia (#46)
    Summary:
    OpenBMC machine Sandia creation
    
    Create machine Sandia to build OpenBMC running on Sandia runBMC. Reference Fuji for creation. Build instruction:
    
    1. Set up OpenBMC build environment.
    2. Run "source open-init-build-env sandia". It goes into build directory.
    3. From build directory, edit conf/local.conf to add a line "INHERIT += "fb-proprietary-recipes" at the end of file.
    4. From build directory, run "bitbake sandia-image".
    
    X-link: https://github.com/facebookexternal/openbmc.cisco/pull/46
    
    Reviewed By: tao-ren, GoldenBug
    
    Differential Revision: D38800418
    
    Pulled By: binhuang00
    
    fbshipit-source-id: 189728a5dbee4ab2168fd671263317f26ddd9f05

---
+++ include/configs/facebook_sandia.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <configs/fbobmc-ast-g6.h>
+
+#endif	/* __CONFIG_H */

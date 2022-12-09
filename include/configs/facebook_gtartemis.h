b7a2301ac56e40f8d43a6052d54bef00fccd0505
Dan Zhang <zhdaniel@meta.com>
2022-12-08 16:06:56 -0800

    Create the skeleton meta-gtartemis layer
    Reviewed By: peterdelevoryas
    
    Differential Revision: D41857401
    
    fbshipit-source-id: b9acde0f96c4a1681f65dbc4dd7c8026e991ef17

---
+++ include/configs/facebook_gtartemis.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2022 Facebook Inc.
+ */
+#ifndef __FACEBOOK_GTARTEMIS_CONFIG_H
+#define __FACEBOOK_GTARTEMIS_CONFIG_H
+
+#define CONFIG_FBGTARTEMIS    1
+#define CONFIG_CMD_MEMTEST2 1
+#include <configs/fbobmc-ast-g6.h>
+
+#endif	/* __FACEBOOK_GTARTEMIS_CONFIG_H */

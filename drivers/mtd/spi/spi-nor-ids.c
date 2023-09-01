a5cd2f055d2bc8883017686c0d089c1afac4ccfa
Ken Chen <j220584470k@gmail.com>
2023-09-01 15:14:43 -0700

    uboot2019.04 & kernel5.15: support for GD55B01GF flash
    Summary:
    # Description
    
    uboot2019.04 & kernel5.15: support for GD55B01GF flash
    
    # Motivation
    
    support GD55B01GF flash as YV35 BSM 3rd source.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4055
    
    Reviewed By: amithash
    
    Differential Revision: D48901600
    
    fbshipit-source-id: 24752eef8cc892a8ce5cf61937f585984e1e1140

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -127,6 +127,11 @@ const struct flash_info spi_nor_ids[] = {
 			SPI_NOR_HAS_TB)
 	},
 	{
+		INFO("gd55b01gf", 0xc8401b, 0, 64 * 1024, 2048,
+			SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_HAS_LOCK |
+			SPI_NOR_HAS_TB)
+	},
+	{
 		INFO("gd55b02ge", 0xc8471c, 0, 64 * 1024, 4096,
 			SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_HAS_LOCK |
 			SPI_NOR_HAS_TB)

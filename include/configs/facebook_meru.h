623c337b7d7221bdb6a0b2e89ddcadfcf1857247
Matthew Parker <mparker@arista.com>
2023-10-30 09:57:55 -0700

    Upstreaming meta-aspeed u-boot-v2019.04
    Summary:
    # Description
    
    Upstreaming the meta-aspeed u-boot-v2019.04 for the new meta-meru branch.
    
    X-link: https://github.com/facebookexternal/openbmc.arista/pull/315
    
    Reviewed By: tao-ren
    
    Differential Revision: D50798620
    
    Pulled By: joancaneus
    
    fbshipit-source-id: d06b409d58eaea84fb5e82d1192d1b1a8f27b4f7

---
+++ include/configs/facebook_meru.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2023 Facebook Inc.
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_MERU    1
+#define CONFIG_CMD_MEMTEST2 1
+#include <configs/fbobmc-ast-g6.h>
+
+#endif	/* __CONFIG_H */
